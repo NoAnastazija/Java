c4a9533a1ad33bb68fa6aa257d7f347476339b5b
Anastazija <106736424+NoAnastazija@users.noreply.github.com>
2024-09-25 19:44:13 +0200

    Add files via upload

---
+++ whilr.java
@@ -0,0 +1,11 @@
+// Prints numbers from 0 - 20
+public class Main{
+public static void main(String args[]){
+  int i = 0;
+  while(i < 21){
+    System.out.println("The value of i is " + i);
+    i++;
+  }
+}
+
+}
