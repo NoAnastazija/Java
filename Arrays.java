056819276e8fd085e2939146e4cca8d35614d333
Anastazija <106736424+NoAnastazija@users.noreply.github.com>
2024-09-25 19:43:44 +0200

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,9 @@
+// Shopping list :D 
+public class Main{
+public static void main(String args[]){
+  String[] myItems = {"Bread","Potatoes","Tomatoes","Milk","Cheese","Cookies"};
+  for(int i=0; i < myItems.length; i++){
+  System.out.println(myItems[i]);
+  }
+}
+}
